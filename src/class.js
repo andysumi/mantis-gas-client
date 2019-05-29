cbc72b14a259d4cc010c9cf0f3f28b4f5a698ea8
andysumi <andysumi@ruby.net.tokai-u.jp>
2019-05-29 13:48:02 +0900

    getSpecificIssueメソッドを追加

--- src/class.js
+++ src/class.js
@@ -6,6 +6,25 @@
       if (!token) throw new Error('"token"は必須です');
     }
 
+    MantisClient.prototype.getSpecificIssue = function (issueId) {
+      if (!issueId) throw new Error('"issueId"は必須です');
+
+      var results = this.fetch_(Utilities.formatString('/issues/%d', issueId), { 'method': 'get' });
+      return results.issues[0];
+    };
+
+    MantisClient.prototype.fetch_ = function (endPoint, options) {
+      var url = this.apiUrl + endPoint;
+      var response = UrlFetchApp.fetch(url, {
+        'method': options.method,
+        'muteHttpExceptions': true,
+        'contentType': 'application/json; charset=utf-8',
+        'headers': this.headers,
+        'payload': options.payload || {}
+      });
+      return JSON.parse(response.getContentText('utf-8'));
+    };
+
     return MantisClient;
   })();
   return global.MantisClient = MantisClient;
--- src/main.js
+++ src/main.js
@@ -6,3 +6,12 @@
 function create(token) { // eslint-disable-line no-unused-vars
   return new MantisClient(token);
 }
+
+/**
+ * Issueの情報を取得する
+ * @param {Integer} issueId 【必須】IssueのID
+ * @return {Object} Issueのオブジェクト
+ */
+function getSpecificIssue(issueId) { // eslint-disable-line no-unused-vars
+  throw new Error('このメソッドは直接呼び出せません。createメソッドで取得したインスタンスより呼び出してください。');
+}
